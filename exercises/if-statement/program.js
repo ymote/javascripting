9bde6c8007ba58cde1d0c7863a309a500349fbfb
Yue Chen <dspfac@gmail.com>
2015-07-25 07:23:18 +0000

    commit exercise content by Live4Code

--- exercises/if-statement/program.js
+++ exercises/if-statement/program.js
@@ -1,7 +1,7 @@
 var fruit = 'orange';
 
 // enter your code below
-
+if(fruit.length > 5){
   console.log('The fruit name has more than five characters.');
 } else {
   console.log('The fruit name has five characters or less.');
